620c5faae6c996211ed97180a951a3837171aa66
Socketlike <36399055+Socketlike@users.noreply.github.com>
2024-04-26 14:12:42 +0700

    feat(ci/deploy): add indexing

--- lune.config.js
+++ lune.config.js
@@ -2,8 +2,6 @@
 // You can view documentation on Lune here:
 // https://github.com/uwu/shelter/tree/main/packages/lune
 
-import { defineConfig } from "@uwu/lune";
+import { defineConfig } from '@uwu/lune'
 
-export default defineConfig({
-  // configure lune here
-});
+export default defineConfig({})
---
+++ scripts/createIndex.js
@@ -0,0 +1,42 @@
+/**
+ * meant to be run after `lune ci`.
+ * any other scenarios are not considered.
+ */
+
+import fs from 'fs/promises'
+import path from 'path'
+
+/** @type {Record<string, { name?: string, author?: string, description?: string, hash?: string }>} */
+const index = await fs
+  .readdir('./dist')
+  .then((plugins) =>
+    Promise.all(
+      plugins.map(async (plugin) => ({
+        name: plugin,
+        manifest:
+          (await fs
+            .readFile(path.join('./dist', plugin, 'plugin.json'))
+            .then((buf) => JSON.parse(buf.toString('utf8')))
+            .catch((e) => {
+              console.error('could not index', plugin, '\n', e)
+            })) || {},
+      })),
+    ),
+  )
+  .then((plugins) =>
+    plugins.reduce((acc, { name, manifest }) => {
+      acc[name] = manifest
+
+      return acc
+    }, {}),
+  )
+  .catch((e) => {
+    console.error('could not create index\n', e)
+  })
+
+if (index)
+  try {
+    await fs.writeFile('./dist/index.json', JSON.stringify(index))
+  } catch (e) {
+    console.log('could not write index\n', e)
+  }
