5136dc4b90a9ca98c0d622195bec3a0e618d33a3
Socketlike <Socketlike@users.noreply.github.com>
2024-06-23 05:26:03 +0000

    deploy: ca059d3b0719ee7ada9f81059393b373cb822629

---
+++ spotify-modal/plugin.js
@@ -0,0 +1,32 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // plugins/spotify-modal/index.jsx
+  var spotify_modal_exports = {};
+  __export(spotify_modal_exports, {
+    onLoad: () => onLoad,
+    onUnload: () => onUnload
+  });
+  var {} = shelter;
+  function onLoad() {
+  }
+  function onUnload() {
+  }
+  return __toCommonJS(spotify_modal_exports);
+})();
